f3d884983818dcf8037cc3e858d0f75fd8b5eaff
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2022-08-25 14:33:52 -0300

    validator: exported cnpj validator

--- packages/validate/src/validators/index.js
+++ packages/validate/src/validators/index.js
@@ -3,6 +3,7 @@ module.exports = {
   ...require('./is-password-strong-validator'),
   ...require('./is-required-validator'),
   ...require('./is-too-long-validator'),
+  ...require('./is-valid-cnpj-validator'),
   ...require('./is-valid-cpf-validator'),
   ...require('./is-valid-email-validator'),
 };
