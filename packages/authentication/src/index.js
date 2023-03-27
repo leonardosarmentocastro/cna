6b369d8dd4909bd66a74b6b872c976d6b14368e6
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2023-03-27 17:12:10 -0300

    (authentication) now exporting resolvers

--- packages/authentication/src/index.js
+++ packages/authentication/src/index.js
@@ -2,11 +2,25 @@ import twoFactor from './2FA/index.js';
 import { connect } from './connect.js';
 import { authenticationMiddleware } from './middleware/index.js';
 import { authenticationSchema } from './schema.js';
+import {
+  meResolver,
+  serveMeResolver,
+  signInResolver,
+  signOutResolver,
+  signTokenResolver,
+  signUpResolver,
+} from './resolvers/index.js';
 
 export {
   authenticationMiddleware,
   authenticationSchema,
   connect,
+  meResolver,
+  serveMeResolver,
+  signInResolver,
+  signOutResolver,
+  signTokenResolver,
+  signUpResolver,
   twoFactor,
 };
 
@@ -14,5 +28,11 @@ export default {
   authenticationMiddleware,
   authenticationSchema,
   connect,
+  meResolver,
+  serveMeResolver,
+  signInResolver,
+  signOutResolver,
+  signTokenResolver,
+  signUpResolver,
   twoFactor,
 };
