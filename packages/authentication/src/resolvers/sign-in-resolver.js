ea193010018b4c0a45810d30699369b8c9c3fbd9
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2024-06-24 11:19:01 -0300

    [authentication] debugging

--- packages/authentication/src/resolvers/sign-in-resolver.js
+++ packages/authentication/src/resolvers/sign-in-resolver.js
@@ -10,18 +10,25 @@ import {
 
 export const signInResolver = (model = DEFAULTS.model) => async (req, res, next) => {
   try {
+    console.debug('@@@ signInResolver')
+
     const constraints = [ ...[ 'cellphoneNumber', 'password' ].map(field => isRequiredValidator(field)) ];
     const err = await validate(constraints, req.body);
+    console.debug('@@@ 1. err', err)
     if (err) throw { err, statusCode: 400 };
 
     const { cellphoneNumber, password } = req.body;
+    console.debug('@@@ 2.0. cellphoneNumber, password', cellphoneNumber, password)
     const doc = await model.findOne({ 'authentication.cellphoneNumber': cellphoneNumber });
+    console.debug('@@@ 2.1. doc', doc)
     if (!doc) throw { err: authenticationErrorCellphoneNumberNotFound(cellphoneNumber), statusCode: 404 };
 
     const hasMatched = await encrypter.verify(doc.authentication.password, password);
+    console.debug('@@@ 3. hasMatched', hasMatched)
     if (!hasMatched) throw { err: authenticationErrorPasswordMismatch(password), statusCode: 404 };
 
     const transformedDoc = doc.toObject({ sensitive: true });
+    console.debug('@@@ 3. transformedDoc', transformedDoc)
     req.signedInDoc = transformedDoc;
     next();
   } catch ({ err, statusCode }) {
